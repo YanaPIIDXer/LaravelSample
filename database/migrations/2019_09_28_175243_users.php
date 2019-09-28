edc9b8f03950a75e8252cd3c2ce5f526abd17601
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-29 03:09:24 +0900

    migrationを作成。

---
+++ database/migrations/2019_09_28_175243_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("user_id");
+            $table->string("password");
+            $table->timestamps();
+        });
+
+        DB::statement("alter table users modify user_id varchar(16) binary");
+        DB::statement("alter table users modify password varchar(256) binary");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
