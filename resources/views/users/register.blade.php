3575c56351af53ff87eabe20bd374b624c26d0a2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-09-30 22:37:58 +0900

    ユーザ登録ページのViewを作成。

---
+++ resources/views/users/register.blade.php
@@ -0,0 +1,14 @@
+@section("Title")
+ユーザ登録
+@endsection
+
+@section("Content")
+<form METHOD="POST" action="{{url('/user_register')}}">
+    {{ csrf_field() }}
+    ユーザＩＤ：<input type="text" name="user_id"><br />
+    パスワード：<input type="password" name="password"><br />
+    <input type="submit" value="登録">
+</form>
+<br />
+<a href="{{url('/')}}">戻る</a>
+@endsection
